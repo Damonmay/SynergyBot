a82951817d052cbe984a68b808138c52546d0444
Damonmay <Damonmay3@gmail.com>
2022-09-05 20:47:20 -0400

    Add files via upload

---
+++ synergybot.py
@@ -0,0 +1,34 @@
+# Created 20220714
+# Discord bot
+# First Priority to recognize the word Synergy
+
+
+import discord
+# import synergyWords.txt
+from discord.ext import commands
+
+client = commands.Bot(command_prefix = '!')
+
+@client.event
+async def on_ready():
+	print("I'm ALIVE")
+
+# @client.command()
+# async def hello(ctx):
+# 	await ctx.send("Hello, I'm the Synergy bot")
+
+
+@client.event
+async def on_message(message):
+
+    messageContent = message.content.lower()
+
+    if 'synergy' in messageContent:
+    	await message.channel.send("SYNERGY") #maybe add some variable statements here
+
+
+client.run('OTk3Mjg4NTIzMzQ0Mzc5OTQ0.Gs1YG8.lXe-zL5pXXbEC9FmVxKEOR5xn-vdoVXjTtIqNw')
+
+
+
+
